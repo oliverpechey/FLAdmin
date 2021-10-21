fdecc26960c3261efb1f45bf9045b5fe52e50411
Raikkonen <40522943+ollieraikkonen@users.noreply.github.com>
2021-10-21 15:23:19 +0100

    Add dotenv and move port params to .env file

--- index.js
+++ index.js
@@ -7,57 +7,57 @@ This requires an MQTT server as well as FLHook 2.1.0 running the MQTT Plugin.
 var express = require("express");
 const path = require("path");
 var app = express();
+require('dotenv').config();
 
 // Start MQTT server
-const aedes = require('aedes')()
-const server = require('net').createServer(aedes.handle)
-const port = 1883
-server.listen(port, function () {
-  console.log('MQTT started and listening on port ', port)
+const aedes = require('aedes')();
+const server = require('net').createServer(aedes.handle);
+server.listen(process.env.MQTT_PORT, function () {
+  console.log('MQTT listening on port', process.env.MQTT_PORT);
 })
 
 // Start MQTT Web Socket server (for browser)
-const httpServer = require('http').createServer()
-const WebSocket = require('ws')
-const wsPort = 8080
+const httpServer = require('http').createServer();
+const WebSocket = require('ws');
+const wsPort = process.env.MQTT_WS_PORT;
 
 const wss = new WebSocket.Server({ server: httpServer })
 wss.on('connection', function connection (ws) {
-  const duplex = WebSocket.createWebSocketStream(ws)
-  aedes.handle(duplex)
+  const duplex = WebSocket.createWebSocketStream(ws);
+  aedes.handle(duplex);
 })
 
 httpServer.listen(wsPort, function () {
-  console.log('Websocket MQTT server listening on port', wsPort)
+  console.log('Websocket MQTT server listening on port', wsPort);
 })
 
 // MQTT Callbacks
 
 aedes.on('clientError', function (client, err) {
-  console.log('MQTT client error:', client.id, err.message, err.stack)
+  console.log('MQTT client error:', client.id, err.message, err.stack);
 })
 
 aedes.on('connectionError', function (client, err) {
-  console.log('MQTT Cconnection error:', client, err.message, err.stack)
+  console.log('MQTT Cconnection error:', client, err.message, err.stack);
 })
 
 aedes.on('publish', function (packet, client) {
   if (packet && packet.payload) {
-    console.log('Publish packet:', packet.payload.toString())
+    console.log('Publish packet:', packet.payload.toString());
   }
   if (client) {
-    console.log('Message from client', client.id)
+    console.log('Message from client', client.id);
   }
 })
 
 aedes.on('subscribe', function (subscriptions, client) {
   if (client) {
-    console.log('subscribe from client', subscriptions, client.id)
+    console.log('subscribe from client', subscriptions, client.id);
   }
 })
 
 aedes.on('client', function (client) {
-  console.log('new client', client.id)
+  console.log('new client', client.id);
 })
 
 // Links to modules and directories for use in html files
@@ -80,6 +80,6 @@ app.get("/", (req, res) => {
 });
 
 // Standard http listening. TODO Need to implement https
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+app.listen(process.env.WEB_PORT,function(){
+  console.log(`Live at Port ${process.env.WEB_PORT}`);
 });
