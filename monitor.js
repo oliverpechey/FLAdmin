4720b95e0d523b70cc13139a78ec84e3d90afb83
Raikkonen <40522943+ollieraikkonen@users.noreply.github.com>
2021-10-22 11:12:58 +0100

    Restart server is over certain ram

--- monitor.js
+++ monitor.js
@@ -1,5 +1,7 @@
 var exec = require('child_process').exec;
 var parent = require.main.exports;
+require('dotenv').config();
+var restart_ram = process.env.RESTART_RAM;
 
 function checkServer() {
     exec('tasklist | find "FLServer.exe"', function(err, stdout, stderr) {
@@ -7,8 +9,16 @@ function checkServer() {
         if(!stdout)
             StartServer(); 
         else {
+            // Get the memory usage from the tasklist line and convert to MB
             var memory = parseInt(stdout.match(/[[0-9]{0,},[0-9]{0,}]{0,}/)[0].replace(',', '').replace('.', '')) / 1024;
+            
+            // If ram is over a certain amount, restart the server
+            if(restart_ram > 0 && memory > restart_ram)
+                RestartServer();
+
             memory = Math.round(memory).toString();
+
+            // Publish ram uage to the memory topic
             parent.mqtt.aedes.publish(
                 {
                     cmd: 'publish',
@@ -22,7 +32,26 @@ function checkServer() {
 }
 
 function StartServer() {
-    
+    exec(process.env.FLServer, function(err, stdout, stderr) {
+        if(!stdout || stderr)
+            console.log('Couldn\'t start server');
+        else
+            console.log('Server started');
+    });
+}
+
+function StopServer() {
+    exec('taskkill /IM "FLServer.exe" /F', function(err, stdout, stderr) {
+        if(!stdout || stderr)
+            console.log('Couldn\'t stop server');
+        else
+            console.log('Server stopped');
+    });
+}
+
+function RestartServer() {
+    StopServer();
+    StartServer();
 }
 
 setInterval(checkServer,5000);
